826cae994b34434c148d229a3f5a884704270bc3
Milan Fox <31571437+MilanFox@users.noreply.github.com>
2026-09-24 17:50:30 +0200

    chore: drop the lint ignore for the deleted audit folder

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'coverage', 'audit'] },
+  { ignores: ['dist', 'node_modules', 'coverage'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
